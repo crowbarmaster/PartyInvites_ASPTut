f146c7fd5cff3d76874d62f1a838dc575b281e64
crowbarmaster <crowbarmasterhl2@gmail.com>
2021-11-17 13:14:51 -0500

    Project finished, works well in .NET 6.0.100

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,36 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
+using System.Linq;
 
-namespace PartyInvites.Controllers;
-
-public class HomeController : Controller
+namespace PartyInvites.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
     }
 }
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "You must include your name!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must inclue a properly formmated email address!")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please include a phone number:")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify if you will intend!")]
+        public bool? WillAttend { get; set; }
+    }
+}
